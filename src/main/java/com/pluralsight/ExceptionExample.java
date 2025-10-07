5ec9909cf7f09a23638071c96b1ab631f146ac3e
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-10-07 14:10:08 -0400

    initial commit

--- src/main/java/com/pluralsight/ExceptionExample.java
+++ src/main/java/com/pluralsight/ExceptionExample.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class ExceptionExample {
+
+    public static void main(String[] args) {
+        String[] students = {"Mario", "Malika", "Nam Kiet", "Mateo"};
+        for (String studentName: students) {
+            System.out.println(studentName);
+        }
+    }
 }
